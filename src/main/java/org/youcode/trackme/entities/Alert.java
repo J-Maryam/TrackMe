5195fbbc3cf8a8d3191c79933e79d1a640db67af
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2025-02-11 12:08:29 +0100

    feat: added Alert entity

--- src/main/java/org/youcode/trackme/entities/Alert.java
+++ src/main/java/org/youcode/trackme/entities/Alert.java
@@ -1,4 +1,39 @@
 package org.youcode.trackme.entities;
 
-public class Location {
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.youcode.trackme.entities.enums.AlertStatus;
+
+import java.time.LocalDateTime;
+
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Alert {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String message;
+
+    @NotNull
+    @Column(nullable = false)
+    private LocalDateTime timestamp;
+
+    @ManyToOne
+    @JoinColumn(name = "patient_id")
+    private Patient patient;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private AlertStatus status;
 }
