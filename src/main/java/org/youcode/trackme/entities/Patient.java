296b742780c7399f16252d355cbeaeae914570bc
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2025-02-11 10:09:49 +0100

    feat: added Patient entity

--- src/main/java/org/youcode/trackme/entities/Patient.java
+++ src/main/java/org/youcode/trackme/entities/Patient.java
@@ -1,4 +1,41 @@
 package org.youcode.trackme.entities;
 
-public class User {
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Past;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Patient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Past
+    private Date dateOfBirth;
+
+    @Transient
+    private int age;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "bracelet_id", referencedColumnName = "id")
+    private Bracelet bracelet;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User caregiver;
 }
