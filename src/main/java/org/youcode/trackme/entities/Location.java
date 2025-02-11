61699ffd6137ea888ca39426326b59b979e051d2
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2025-02-11 11:43:45 +0100

    feat: added Location entity

--- src/main/java/org/youcode/trackme/entities/Location.java
+++ src/main/java/org/youcode/trackme/entities/Location.java
@@ -1,4 +1,37 @@
 package org.youcode.trackme.entities;
 
-public class Bracelet {
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Location {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Double latitude;
+
+    @Column(nullable = false)
+    private Double longitude;
+
+    @Column(nullable = false)
+    private LocalDateTime timestamp;
+
+    @ManyToOne
+    @JoinColumn(name = "bracelet_id")
+    private Bracelet bracelet;
+
+    @ManyToOne
+    @JoinColumn(name = "patient_id")
+    private Patient patient;  // Le patient associé
 }
