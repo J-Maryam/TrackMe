779208143d4ab9924842cc2ea43a410f74fd3092
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2025-02-11 10:33:04 +0100

    feat: added BraceletStatus enum and Bracelet entity

--- src/main/java/org/youcode/trackme/entities/Bracelet.java
+++ src/main/java/org/youcode/trackme/entities/Bracelet.java
@@ -1,4 +1,37 @@
 package org.youcode.trackme.entities;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.youcode.trackme.entities.enums.BraceletStatus;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Bracelet {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    @NotNull
+    private String serialNumber;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private BraceletStatus status;
+
+    @OneToOne(mappedBy = "bracelet")
+    private Patient patient;
+
+    @OneToMany(mappedBy = "bracelet")
+    private List<Location> locations = new ArrayList<>();
 }
---
+++ src/main/java/org/youcode/trackme/entities/enums/BraceletStatus.java
@@ -0,0 +1,9 @@
+package org.youcode.trackme.entities.enums;
+
+public enum BraceletStatus {
+    ACTIVE,       // Bracelet allumé et connecté
+    INACTIVE,     // Bracelet éteint ou désactivé
+    LOW_BATTERY,  // Batterie faible
+    NO_SIGNAL,    // Aucune connexion
+    SOS_TRIGGERED // Alerte SOS envoyée
+}
