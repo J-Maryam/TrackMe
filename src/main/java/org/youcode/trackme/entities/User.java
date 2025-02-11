782447aeec0f072413124dd1f40b135339e634c2
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2025-02-11 10:05:08 +0100

    feat: added user entity

--- src/main/java/org/youcode/trackme/entities/User.java
+++ src/main/java/org/youcode/trackme/entities/User.java
@@ -1,4 +1,50 @@
 package org.youcode.trackme.entities;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.youcode.trackme.entities.enums.Role;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Username is mandatory")
+    @Size(min = 3, max = 50, message = "Username must be between 3 and 50 characters")
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @NotBlank(message = "Password is mandatory")
+    @Size(min = 6, max = 100, message = "Password must be at least 6 characters long")
+    @Column(nullable = false)
+    private String password;
+
+    @NotBlank(message = "Email is mandatory")
+    @Email(message = "Email should be valid")
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    private String phoneNumber;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role; // Enum: ADMIN, CAREGIVER
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Patient> patients = new ArrayList<>();
 }
