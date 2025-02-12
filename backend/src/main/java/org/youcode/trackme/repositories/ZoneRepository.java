be297ac26bc04b10fe140d7d4b4bf39e55fdb372
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2025-02-12 23:18:28 +0100

    feat: added dependenciees in pom.xml file

--- backend/src/main/java/org/youcode/trackme/repositories/ZoneRepository.java
+++
@@ -1,9 +0,0 @@
-package org.youcode.trackme.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import org.youcode.trackme.entities.Geofence;
-
-@Repository
-public interface ZoneRepository extends JpaRepository<Geofence, Long> {
-}
