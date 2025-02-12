df0f30d5f7384c1029f901d434c92dbb3f98a278
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2025-02-12 10:59:49 +0100

    feat: added user request dto

---
+++ backend/src/main/java/org/youcode/trackme/dtos/user/UserRequestDTO.java
@@ -0,0 +1,24 @@
+package org.youcode.trackme.dtos.user;
+
+import jakarta.validation.constraints.*;
+
+public record UserRequestDTO(
+
+        @NotBlank(message = "Username is mandatory")
+        @Size(min = 3, max = 50, message = "Username must be between 3 and 50 characters")
+        String username,
+
+        @NotBlank(message = "Password is mandatory")
+        @Size(min = 6, max = 100, message = "Password must be at least 6 characters long")
+        String password,
+
+        @NotBlank(message = "Email is mandatory")
+        @Email(message = "Email should be valid")
+        String email,
+
+        @NotNull
+        String phoneNumber,
+
+        Long role
+) {
+}
