82b4bafc0824bb9135ff82e365c3c4d651de30d2
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2025-02-12 23:31:54 +0100

    feat: added user service implementation

--- backend/src/main/java/org/youcode/trackme/services/impls/UserServiceImpl.java
+++ backend/src/main/java/org/youcode/trackme/services/impls/UserServiceImpl.java
@@ -1,4 +1,21 @@
 package org.youcode.trackme.services.impls;
 
-public class UserServiceImpl {
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.validation.annotation.Validated;
+import org.youcode.trackme.common.services.GenericServiceImpl;
+import org.youcode.trackme.dtos.user.UserRequestDTO;
+import org.youcode.trackme.dtos.user.UserResponseDTO;
+import org.youcode.trackme.entities.User;
+import org.youcode.trackme.mappers.UserMapper;
+import org.youcode.trackme.repositories.UserRepository;
+import org.youcode.trackme.services.UserService;
+
+@Service
+@Transactional
+@Validated
+public class UserServiceImpl extends GenericServiceImpl<User, Long, UserRequestDTO, UserResponseDTO> implements UserService {
+    public UserServiceImpl(UserRepository repository, UserMapper mapper) {
+        super(repository, mapper);
+    }
 }
