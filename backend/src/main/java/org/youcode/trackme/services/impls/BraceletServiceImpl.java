bb9da3606c9f10bd82345a89862df3b9e97c963a
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2025-02-13 12:01:37 +0100

    feat(bracelet): added create method in the service to add a new bracelet with a generative serial number, available status and inactive state

--- backend/src/main/java/org/youcode/trackme/services/impls/BraceletServiceImpl.java
+++ backend/src/main/java/org/youcode/trackme/services/impls/BraceletServiceImpl.java
@@ -7,9 +7,13 @@ import org.youcode.trackme.common.services.GenericServiceImpl;
 import org.youcode.trackme.dtos.bracelet.BraceletRequestDTO;
 import org.youcode.trackme.dtos.bracelet.BraceletResponseDTO;
 import org.youcode.trackme.entities.Bracelet;
+import org.youcode.trackme.entities.enums.BraceletState;
+import org.youcode.trackme.entities.enums.BraceletStatus;
 import org.youcode.trackme.mappers.BraceletMapper;
 import org.youcode.trackme.repositories.BraceletRepository;
 import org.youcode.trackme.services.BraceletService;
+
+import java.util.UUID;
 
 @Service
 @Transactional
@@ -23,5 +27,27 @@ public class BraceletServiceImpl extends GenericServiceImpl<Bracelet, Long, Brac
         super(repository, mapper);
         this.braceletRepository = repository;
         this.braceletMapper = mapper;
+    }
+
+    @Override
+    public BraceletResponseDTO create(BraceletRequestDTO requestDto) {
+        String serialNumber = generateSerialNumber();
+
+        Bracelet bracelet = new Bracelet();
+        bracelet.setSerialNumber(serialNumber);
+        bracelet.setStatus(BraceletStatus.INACTIVE);
+        bracelet.setState(BraceletState.AVAILABLE);
+
+        Bracelet savedBracelet = braceletRepository.save(bracelet);
+
+        return braceletMapper.toDto(savedBracelet);
+    }
+
+    /**
+     * Méthode pour générer un numéro de série unique au format BRAC-XXXXXX
+     */
+    private String generateSerialNumber() {
+        String randomPart = UUID.randomUUID().toString().substring(0, 8).toUpperCase();
+        return "BRAC-" + randomPart;
     }
 }
