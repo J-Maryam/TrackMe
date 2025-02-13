537699bbbae938cf9bab4c5ac4f9b2c377b5c928
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2025-02-13 11:50:05 +0100

    feat(bracelet): added bracelet controller

---
+++ backend/src/main/java/org/youcode/trackme/controllers/BraceletController.java
@@ -0,0 +1,17 @@
+package org.youcode.trackme.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.youcode.trackme.common.controllers.GenericControllerImpl;
+import org.youcode.trackme.dtos.bracelet.BraceletRequestDTO;
+import org.youcode.trackme.dtos.bracelet.BraceletResponseDTO;
+import org.youcode.trackme.entities.Bracelet;
+import org.youcode.trackme.services.BraceletService;
+
+@RestController
+@RequestMapping("/bracelets")
+public class BraceletController extends GenericControllerImpl<Bracelet, Long, BraceletRequestDTO, BraceletResponseDTO> {
+    public BraceletController(BraceletService service) {
+        super(service);
+    }
+}
