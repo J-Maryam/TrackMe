60d1c3931f3cdfa5c23a5ebb8567e2f39f3911d4
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2025-02-13 11:42:18 +0100

    feat: added bracelet repository

--- backend/src/main/java/org/youcode/trackme/repositories/BraceletRepository.java
+++ backend/src/main/java/org/youcode/trackme/repositories/BraceletRepository.java
@@ -1,7 +1,9 @@
 package org.youcode.trackme.repositories;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.youcode.trackme.entities.Bracelet;
 
 @Repository
-public interface BraceletRepository {
+public interface BraceletRepository extends JpaRepository<Bracelet, Long> {
 }
