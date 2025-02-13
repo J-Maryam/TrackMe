ad666416db86361f5a9499b61f0cadbcb3fce721
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2025-02-13 23:52:36 +0100

    feat: added payment response dto

--- backend/src/main/java/org/youcode/trackme/dtos/payment/PaymentResponseDTO.java
+++ backend/src/main/java/org/youcode/trackme/dtos/payment/PaymentResponseDTO.java
@@ -1,4 +1,14 @@
 package org.youcode.trackme.dtos.payment;
 
-public record PaymentResponseDTO() {
-}
+import org.youcode.trackme.entities.Order;
+import org.youcode.trackme.entities.enums.PaymentStatus;
+
+import java.time.LocalDateTime;
+
+public record PaymentResponseDTO(
+        Long id,
+        Order order,
+        PaymentStatus status,
+        LocalDateTime paymentDate,
+        String transactionId
+) {}
