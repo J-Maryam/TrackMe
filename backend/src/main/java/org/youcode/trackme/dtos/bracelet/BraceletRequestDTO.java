e0e903aa7727035e1e1118c2d480ed6a959fb772
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2025-02-13 11:37:31 +0100

    feat: added bracelet request dto

--- backend/src/main/java/org/youcode/trackme/dtos/bracelet/BraceletRequestDTO.java
+++ backend/src/main/java/org/youcode/trackme/dtos/bracelet/BraceletRequestDTO.java
@@ -1,4 +1,18 @@
 package org.youcode.trackme.dtos.bracelet;
 
-public record BraceletRequestDTO() {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import org.youcode.trackme.entities.enums.BraceletState;
+import org.youcode.trackme.entities.enums.BraceletStatus;
+
+public record BraceletRequestDTO(
+        @NotBlank(message = "Serial number is mandatory")
+        String serialNumber,
+
+        @NotNull(message = "Status is mandatory")
+        BraceletStatus status,
+
+        @NotNull(message = "State is mandatory")
+        BraceletState state
+) {
 }
