e264148a078a04ea38ea5224b0a4707943c5c6c4
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2025-02-13 11:37:46 +0100

    feat: added bracelet response dto

--- backend/src/main/java/org/youcode/trackme/dtos/bracelet/BraceletResponseDTO.java
+++ backend/src/main/java/org/youcode/trackme/dtos/bracelet/BraceletResponseDTO.java
@@ -1,4 +1,17 @@
 package org.youcode.trackme.dtos.bracelet;
 
-public record BraceletResponseDTO() {
-}
+import org.youcode.trackme.entities.Patient;
+import org.youcode.trackme.entities.enums.BraceletState;
+import org.youcode.trackme.entities.enums.BraceletStatus;
+
+public record BraceletResponseDTO(
+        Long id,
+
+        String serialNumber,
+
+        BraceletStatus status,
+
+        BraceletState state,
+
+        Patient patient
+) {}
