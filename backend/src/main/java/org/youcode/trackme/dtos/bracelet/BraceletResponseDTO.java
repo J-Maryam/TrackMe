29a37e53f3fb2adfc550c45aa9e7075f705d51c2
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2025-02-13 11:26:02 +0100

    feat: added BraceletState as a enum type

---
+++ backend/src/main/java/org/youcode/trackme/dtos/bracelet/BraceletResponseDTO.java
@@ -0,0 +1,4 @@
+package org.youcode.trackme.dtos.bracelet;
+
+public record BraceletResponseDTO() {
+}
---
+++ backend/src/main/java/org/youcode/trackme/entities/enums/BraceletState.java
@@ -0,0 +1,6 @@
+package org.youcode.trackme.entities.enums;
+
+public enum BraceletState {
+    AVAILABLE,
+    ASSIGNED
+}
