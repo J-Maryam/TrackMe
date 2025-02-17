53b49b087a88019d126370c08b060e5cade2bf6b
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2025-02-17 11:35:32 +0100

    refactor: separated how it works section of the homepage

--- frontend/src/app/features/home/home.component.ts
+++ frontend/src/app/features/home/home.component.ts
@@ -5,13 +5,14 @@ import {HeaderComponent} from '../../shared/components/header/header.component';
 import {FooterComponent} from '../../shared/components/footer/footer.component';
 import {HeroSectionComponent} from './hero-section/hero-section.component';
 import {FonctionnalitiesSectionComponent} from './fonctionnalities-section/fonctionnalities-section.component';
+import {HowItWorksSectionComponent} from './how-it-works-section/how-it-works-section.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
   standalone: true,
-  imports: [CommonModule, RouterModule, HeaderComponent, FooterComponent, NgOptimizedImage, HeroSectionComponent, FonctionnalitiesSectionComponent]
+  imports: [CommonModule, RouterModule, HeaderComponent, FooterComponent, NgOptimizedImage, HeroSectionComponent, FonctionnalitiesSectionComponent, HowItWorksSectionComponent]
 })
 export class HomeComponent {
 }
