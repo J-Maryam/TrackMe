7e8c6fe4e47be65b91929d2943e9d983f93e286a
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2025-02-17 10:54:52 +0100

    refactor: update homepage pic

--- frontend/src/app/features/home/home.component.ts
+++ frontend/src/app/features/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { CommonModule } from '@angular/common';
+import {CommonModule, NgOptimizedImage} from '@angular/common';
 import {HeaderComponent} from '../../shared/components/header/header.component';
 import {FooterComponent} from '../../shared/components/footer/footer.component';
 
@@ -9,7 +9,7 @@ import {FooterComponent} from '../../shared/components/footer/footer.component';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
   standalone: true,
-  imports: [CommonModule, RouterModule, HeaderComponent, FooterComponent]
+  imports: [CommonModule, RouterModule, HeaderComponent, FooterComponent, NgOptimizedImage]
 })
 export class HomeComponent {
 }
