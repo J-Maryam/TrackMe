f723028f593b081a49737fb8b6136e7dc4c3fe44
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2025-02-21 12:58:05 +0100

    feat: added existsByEmail and existsByUsername methods in user repository

--- backend/src/main/java/org/youcode/trackme/repositories/UserRepository.java
+++ backend/src/main/java/org/youcode/trackme/repositories/UserRepository.java
@@ -4,6 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.youcode.trackme.entities.AppUser;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<AppUser, Long> {
+    Optional<AppUser> findByUsername(String username);
+    boolean existsByEmail(String email);
+    boolean existsByUsername(String username);
 }
