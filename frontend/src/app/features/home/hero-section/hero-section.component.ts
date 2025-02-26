d593dfeabd1e5c0d829957081a992b84d012bec0
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2025-02-26 15:06:29 +0000

    feat: added pic of the hero section in the home page

--- frontend/src/app/features/home/hero-section/hero-section.component.ts
+++ frontend/src/app/features/home/hero-section/hero-section.component.ts
@@ -1,15 +1,18 @@
-import { Component } from '@angular/core';
-import {RouterLink} from '@angular/router';
+  import { Component } from '@angular/core';
+  import {RouterLink} from '@angular/router';
+  import {NgOptimizedImage} from '@angular/common';
 
-@Component({
-  selector: 'app-hero-section',
-  imports: [
-    RouterLink
-  ],
-  templateUrl: './hero-section.component.html',
-  standalone: true,
-  styleUrl: './hero-section.component.css'
-})
-export class HeroSectionComponent {
-
-}
+  @Component({
+    selector: 'app-hero-section',
+    imports: [
+      RouterLink,
+      NgOptimizedImage
+    ],
+    templateUrl: './hero-section.component.html',
+    standalone: true,
+    styleUrl: './hero-section.component.css'
+  })
+  export class HeroSectionComponent {
+    // image = "logo_homepage.png";
+    image = 'bracelet-gps.png';
+  }
