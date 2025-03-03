90f83a92a602ebf3fc92f34318204f80297ec410
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2025-03-03 09:39:40 +0000

    feat(payment): added payment dtos

--- backend/src/main/java/org/youcode/trackme/dtos/payment/ConfirmPaymentRequest.java
+++ backend/src/main/java/org/youcode/trackme/dtos/payment/ConfirmPaymentRequest.java
@@ -1,4 +1,7 @@
 package org.youcode.trackme.dtos.payment;
 
-public record ConfirmPaymentRequest() {
-}
+import jakarta.validation.constraints.NotBlank;
+
+public record ConfirmPaymentRequest(
+        @NotBlank String transactionId
+) {}
--- backend/src/main/java/org/youcode/trackme/dtos/payment/PaymentRequestDTO.java
+++ backend/src/main/java/org/youcode/trackme/dtos/payment/PaymentRequestDTO.java
@@ -3,8 +3,7 @@ package org.youcode.trackme.dtos.payment;
 import org.youcode.trackme.entities.enums.PaymentStatus;
 
 public record PaymentRequestDTO(
-        Long orderId,
-        PaymentStatus status,
-        Double amount,
-        String transactionId
+//        PaymentStatus status,
+        Double amount
+//        String transactionId
 ) {}
