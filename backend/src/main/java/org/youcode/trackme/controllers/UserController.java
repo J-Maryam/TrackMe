529e1c04745003f8654df2eef8eba036a98aa02f
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2025-03-03 10:53:49 +0000

    feat(user): added check email-method to check if a user with this email is already exist or not

--- backend/src/main/java/org/youcode/trackme/controllers/UserController.java
+++ backend/src/main/java/org/youcode/trackme/controllers/UserController.java
@@ -3,10 +3,7 @@ package org.youcode.trackme.controllers;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.youcode.trackme.common.controllers.GenericControllerImpl;
 import org.youcode.trackme.dtos.user.UserRequestDTO;
 import org.youcode.trackme.dtos.user.UserResponseDTO;
@@ -14,9 +11,19 @@ import org.youcode.trackme.security.entities.AppUser;
 import org.youcode.trackme.services.UserService;
 
 @RestController
-@RequestMapping("/users")
+//@RequestMapping("/api")
 public class UserController extends GenericControllerImpl<AppUser, Long, UserRequestDTO, UserResponseDTO> {
-    public UserController(UserService service) {
+
+    private final UserService userService;
+
+    public UserController(UserService service, UserService userService) {
         super(service);
+        this.userService = userService;
+    }
+
+    @GetMapping("/api/public/users/check-email")
+    public ResponseEntity<Boolean> checkEmail(@RequestParam String email) {
+        boolean exists = userService.checkEmailExists(email);
+        return ResponseEntity.ok(exists);
     }
 }
