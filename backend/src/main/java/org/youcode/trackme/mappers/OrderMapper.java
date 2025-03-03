ce0ac15b48926a180831fde04d6580176a2a8ac9
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2025-03-03 09:38:34 +0000

    feat: added order mapper

--- backend/src/main/java/org/youcode/trackme/mappers/OrderMapper.java
+++ backend/src/main/java/org/youcode/trackme/mappers/OrderMapper.java
@@ -1,4 +1,11 @@
 package org.youcode.trackme.mappers;
 
-public interface OdrerMapper {
+import org.mapstruct.Mapper;
+import org.youcode.trackme.common.Mapper.GenericMapper;
+import org.youcode.trackme.dtos.order.OrderRequestDTO;
+import org.youcode.trackme.dtos.order.OrderResponseDTO;
+import org.youcode.trackme.entities.Order;
+
+@Mapper(componentModel = "spring")
+public interface OrderMapper extends GenericMapper<Order, OrderRequestDTO, OrderResponseDTO> {
 }
