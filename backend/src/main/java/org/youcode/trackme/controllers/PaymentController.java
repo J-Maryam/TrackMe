220ac764d8624ff6cfb202044cf2e44d1726c48c
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2025-03-03 09:48:36 +0000

    feat(payment): created paymentController and added createPaymentIntent and confirmPayment endpoints

--- backend/src/main/java/org/youcode/trackme/controllers/PaymentController.java
+++ backend/src/main/java/org/youcode/trackme/controllers/PaymentController.java
@@ -1,4 +1,43 @@
 package org.youcode.trackme.controllers;
 
+import com.stripe.exception.StripeException;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.youcode.trackme.dtos.payment.ConfirmPaymentRequest;
+import org.youcode.trackme.dtos.payment.PaymentRequestDTO;
+import org.youcode.trackme.dtos.payment.PaymentResponseDTO;
+import org.youcode.trackme.mappers.PaymentMapper;
+import org.youcode.trackme.services.PaymentService;
+
+@RestController
+@RequestMapping("/api/public/payments")
 public class PaymentController {
+
+    private final PaymentService paymentService;
+    private final PaymentMapper paymentMapper;
+
+    @Autowired
+    public PaymentController(PaymentService paymentService, PaymentMapper paymentMapper) {
+        this.paymentService = paymentService;
+        this.paymentMapper = paymentMapper;
+    }
+
+    @PostMapping("/create-payment-intent")
+    public ResponseEntity<String> createPaymentIntent(@Valid @RequestBody PaymentRequestDTO request) {
+        try {
+            String clientSecret = paymentService.createPaymentIntent(request);
+            return new ResponseEntity<>(clientSecret, HttpStatus.OK);
+        } catch (StripeException e) {
+            return new ResponseEntity<>("Erreur lors de la création du paiement : " + e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PostMapping("/confirm")
+    public ResponseEntity<PaymentResponseDTO> confirmPayment(@Valid @RequestBody ConfirmPaymentRequest request) {
+        PaymentResponseDTO paymentResponse = paymentMapper.toDto(paymentService.confirmPayment(request.transactionId()));
+        return new ResponseEntity<>(paymentResponse, HttpStatus.OK);
+    }
 }
