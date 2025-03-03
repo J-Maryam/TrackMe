fdf8bc707290d2ad90ee04b2ed9eb593ea3b38b1
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2025-03-03 09:44:38 +0000

    feat(payment): added createPaymentIntent and confirmPayment methods in the PaymentService

--- backend/src/main/java/org/youcode/trackme/services/PaymentService.java
+++ backend/src/main/java/org/youcode/trackme/services/PaymentService.java
@@ -1,4 +1,10 @@
 package org.youcode.trackme.services;
 
+import com.stripe.exception.StripeException;
+import org.youcode.trackme.dtos.payment.PaymentRequestDTO;
+import org.youcode.trackme.entities.Payment;
+
 public interface PaymentService {
+    String createPaymentIntent(PaymentRequestDTO payment) throws StripeException;
+    Payment confirmPayment(String transactionId);
 }
