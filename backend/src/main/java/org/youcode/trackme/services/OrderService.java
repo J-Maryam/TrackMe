b6596851767d3ea983ca53d84419d1f4e3339721
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2025-03-03 09:45:44 +0000

    feat(payment): added completeOrder methods in the PaymentService

--- backend/src/main/java/org/youcode/trackme/services/OrderService.java
+++ backend/src/main/java/org/youcode/trackme/services/OrderService.java
@@ -1,4 +1,8 @@
 package org.youcode.trackme.services;
 
+import org.youcode.trackme.dtos.order.OrderRequestDTO;
+import org.youcode.trackme.dtos.order.OrderResponseDTO;
+
 public interface OrderService {
+    OrderResponseDTO completeOrder(OrderRequestDTO request);
 }
