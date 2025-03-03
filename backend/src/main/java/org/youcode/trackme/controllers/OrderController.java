e2f0181b09281630335b2d4d8e85e3d72b8fb634
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2025-03-03 09:49:33 +0000

    feat(order): created OrderController and added completeOrder endpoint

--- backend/src/main/java/org/youcode/trackme/controllers/OrderController.java
+++ backend/src/main/java/org/youcode/trackme/controllers/OrderController.java
@@ -1,4 +1,28 @@
 package org.youcode.trackme.controllers;
 
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.youcode.trackme.dtos.order.OrderRequestDTO;
+import org.youcode.trackme.dtos.order.OrderResponseDTO;
+import org.youcode.trackme.services.OrderService;
+
+@RestController
+@RequestMapping("/api/public/orders")
 public class OrderController {
+
+    private final OrderService orderService;
+
+    @Autowired
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @PostMapping("/complete")
+    public ResponseEntity<OrderResponseDTO> completeOrder(@Valid @RequestBody OrderRequestDTO request) {
+        OrderResponseDTO response = orderService.completeOrder(request);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
 }
