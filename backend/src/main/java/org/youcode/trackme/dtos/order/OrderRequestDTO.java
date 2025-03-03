33561115ff7cdce818bacc6d3ac856dc938d2e50
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2025-03-03 09:39:59 +0000

    feat(payment): added order dtos

--- backend/src/main/java/org/youcode/trackme/dtos/order/OrderRequestDTO.java
+++ backend/src/main/java/org/youcode/trackme/dtos/order/OrderRequestDTO.java
@@ -1,11 +1,12 @@
 package org.youcode.trackme.dtos.order;
 
-import org.youcode.trackme.entities.enums.OrderStatus;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 
 public record OrderRequestDTO(
-        Long userId,
-        Long braceletId,
-        OrderStatus status,
-        double totalAmount,
-        String deliveryAddress
+        @NotNull Long braceletId,
+        @NotNull Long userId,
+        @NotNull @Positive double totalAmount,
+        @NotNull String deliveryAddress,
+        @NotNull String transactionId
 ) {}
--- backend/src/main/java/org/youcode/trackme/dtos/order/OrderResponseDTO.java
+++ backend/src/main/java/org/youcode/trackme/dtos/order/OrderResponseDTO.java
@@ -1,19 +1,23 @@
 package org.youcode.trackme.dtos.order;
 
+import org.youcode.trackme.dtos.bracelet.BraceletResponseDTO;
 import org.youcode.trackme.dtos.bracelet.EmbeddableBraceletResponseDTO;
+import org.youcode.trackme.dtos.patient.PatientResponseDTO;
+import org.youcode.trackme.dtos.payment.EmbeddablePaymentResponseDTO;
 import org.youcode.trackme.dtos.user.EmbeddableUserResponseDTO;
+import org.youcode.trackme.dtos.user.UserResponseDTO;
 import org.youcode.trackme.entities.enums.OrderStatus;
 
 import java.time.LocalDateTime;
 
 public record OrderResponseDTO(
         Long id,
-        EmbeddableUserResponseDTO userId,
-        EmbeddableBraceletResponseDTO braceletId,
+        EmbeddableBraceletResponseDTO bracelet,
+        EmbeddableUserResponseDTO user,
         OrderStatus status,
         double totalAmount,
         LocalDateTime dateCommande,
         LocalDateTime dateLivraison,
         String deliveryAddress,
-        Long paymentId
+        EmbeddablePaymentResponseDTO payment
 ) {}
