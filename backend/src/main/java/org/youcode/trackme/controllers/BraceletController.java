b769476ab75425a97ccc3e7333cb652a056680cd
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2025-03-06 11:43:44 +0000

    feat(bracelet):added cross origin in tha bracelet controller

--- backend/src/main/java/org/youcode/trackme/controllers/BraceletController.java
+++ backend/src/main/java/org/youcode/trackme/controllers/BraceletController.java
@@ -1,5 +1,6 @@
 package org.youcode.trackme.controllers;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.youcode.trackme.common.controllers.GenericControllerImpl;
@@ -10,6 +11,7 @@ import org.youcode.trackme.services.BraceletService;
 
 @RestController
 @RequestMapping("/api/public/bracelets")
+@CrossOrigin(origins = "http://localhost:4200")
 public class BraceletController extends GenericControllerImpl<Bracelet, Long, BraceletRequestDTO, BraceletResponseDTO> {
     public BraceletController(BraceletService service) {
         super(service);
