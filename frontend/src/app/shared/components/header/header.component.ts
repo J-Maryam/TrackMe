8e57b7c234283cd6ad58baf3ecf89786a690e03d
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2025-03-06 21:21:09 +0000

    refactor(header): update header component

--- frontend/src/app/shared/components/header/header.component.ts
+++ frontend/src/app/shared/components/header/header.component.ts
@@ -1,19 +1,16 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 import {LoginComponent} from '../../../features/auth/login/login.component';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   standalone: true,
   imports: [
-    LoginComponent
+    LoginComponent,
+    RouterLink
   ],
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-  @ViewChild('loginPopup') loginPopup!: LoginComponent;
-
-  openLogin() {
-    this.loginPopup.openPopup();
-  }
 }
