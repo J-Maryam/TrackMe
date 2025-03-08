bec5956bf1ff438dc66dd13e1aea64f666c89611
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2025-03-08 12:51:44 +0000

    feat(sidebar): added sidebar component

---
+++ frontend/src/app/shared/components/sidebar/sidebar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar',
+  imports: [],
+  templateUrl: './sidebar.component.html',
+  standalone: true,
+  styleUrl: './sidebar.component.css'
+})
+export class SidebarComponent {
+
+}
