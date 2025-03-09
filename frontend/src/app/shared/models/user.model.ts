e96356a6c76ed978a6e91ab7b0b650ae3a2cb7e4
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2025-03-09 14:33:27 +0000

    feat(user-management): added user management component

--- frontend/src/app/shared/models/user.model.ts
+++ frontend/src/app/shared/models/user.model.ts
@@ -1,11 +1,16 @@
 // Interface pour les données d'entrée de l'utilisateur (email et mot de passe)
 export interface User {
+  id: string;
+  name: string;
   email: string;
-  password: string;
+  password?: string;
+  role?: 'ROLE_ADMIN' | 'ROLE_USER';
+  address?: string;
+  telephone?: string;
 }
 
 // Interface pour la réponse d'authentification du serveur
 export interface AuthResponse {
   token: string;
-  role?: string; // Optionnel : rôle de l'utilisateur (ex. "admin" ou "caregiver")
+  role?: string;
 }
