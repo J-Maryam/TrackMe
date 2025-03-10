3a5cb673cca1bc4835dc12a2a15d1ca55de87c2e
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2025-03-10 22:32:44 +0000

    feat(role): added getAllRoles endpoint in AppRoleController

--- backend/src/main/java/org/youcode/trackme/security/controllers/AppRoleController.java
+++ backend/src/main/java/org/youcode/trackme/security/controllers/AppRoleController.java
@@ -1,4 +1,26 @@
 package org.youcode.trackme.security.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.youcode.trackme.security.dtos.AppRoleDTO.EmbeddableAppRoleDTO;
+import org.youcode.trackme.security.services.implementations.AppRoleService;
+
+import java.util.List;
+
+@Validated
+@RestController
+@RequestMapping("/api")
 public class AppRoleController {
+
+    @Autowired
+    private AppRoleService appRoleService;
+
+    @GetMapping("/admin/roles")
+    public ResponseEntity<List<EmbeddableAppRoleDTO>> getAllRoles() {
+        return ResponseEntity.ok(appRoleService.getAllRoles());
+    }
 }
