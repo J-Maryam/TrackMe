e4925168996f257e7e45d83965403ad1cfdce7af
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2025-03-10 22:29:29 +0000

    feat(service): created AppRole Service interface

--- backend/src/main/java/org/youcode/trackme/security/services/interfaces/IAppRoleService.java
+++ backend/src/main/java/org/youcode/trackme/security/services/interfaces/IAppRoleService.java
@@ -1,4 +1,10 @@
 package org.youcode.trackme.security.services.interfaces;
 
+
+import org.youcode.trackme.security.dtos.AppRoleDTO.EmbeddableAppRoleDTO;
+
+import java.util.List;
+
 public interface IAppRoleService {
+    List<EmbeddableAppRoleDTO> getAllRoles();
 }
