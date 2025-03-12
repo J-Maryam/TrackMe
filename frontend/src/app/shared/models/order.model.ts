84a4b736951e9b221539c660d0fd1ecbb5235022
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2025-03-12 23:08:59 +0000

    feat(model): added order model

--- frontend/src/app/shared/models/order.model.ts
+++ frontend/src/app/shared/models/order.model.ts
@@ -1,8 +1,11 @@
-export interface Bracelet {
-  serialNumber: string;
-  color: string;
-  status: string;
-  patientUsername: string;
-  clientUsername: string;
-  datePaiement: Date;
+import { Bracelet } from './bracelet.model';
+import { User } from './user.model';
+import { Patient } from './patient.model';
+
+export interface Order {
+  id: number;
+  bracelet: Bracelet;
+  user: User;
+  patient: Patient;
+  orderDate: string | null;
 }
