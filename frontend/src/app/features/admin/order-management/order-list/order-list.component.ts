11740c616fe9e1ce635160202faddcff7a087617
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2025-03-13 11:43:41 +0000

    feat(service): added payment service

--- frontend/src/app/features/admin/order-management/order-list/order-list.component.ts
+++ frontend/src/app/features/admin/order-management/order-list/order-list.component.ts
@@ -1,16 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { Order } from '../../../../shared/models/order.model';
 import { OrderService } from '../../../../core/services/order.service';
-import {DatePipe, NgForOf, NgIf} from '@angular/common';
+import { DatePipe, NgForOf, NgIf } from '@angular/common';
 import { SidebarComponent } from '../../../../shared/components/sidebar/sidebar.component';
 import { AuthService } from '../../../../core/services/auth.service';
 import { Router } from '@angular/router';
+import { ModalService } from '../../../../shared/services/modal.service';
+import { OrderDetailsComponent } from '../order-details/order-details.component';
 
 @Component({
   selector: 'app-order-list',
   templateUrl: './order-list.component.html',
   styleUrls: ['./order-list.component.css'],
-  imports: [NgIf, NgForOf, SidebarComponent, DatePipe],
+  imports: [NgIf, NgForOf, SidebarComponent, DatePipe, OrderDetailsComponent],
   standalone: true,
 })
 export class OrderListComponent implements OnInit {
@@ -25,6 +27,7 @@ export class OrderListComponent implements OnInit {
   constructor(
     private orderService: OrderService,
     private authService: AuthService,
+    private modalService: ModalService,
     private router: Router
   ) {}
 
@@ -40,13 +43,12 @@ export class OrderListComponent implements OnInit {
     this.isLoading = true;
     this.errorMessage = null;
 
-    // Page est 0-based pour l'API
     this.orderService.getOrders(this.currentPage - 1, this.pageSize).subscribe({
       next: (response) => {
         this.orders = response.data.content;
         this.totalElements = response.data.totalElements;
         this.totalPages = response.data.totalPages;
-        this.currentPage = response.data.pageNumber + 1; // Convertir 0-based en 1-based
+        this.currentPage = response.data.pageNumber + 1;
         this.isLoading = false;
       },
       error: (err) => {
@@ -69,5 +71,9 @@ export class OrderListComponent implements OnInit {
       this.currentPage++;
       this.loadOrders();
     }
+  }
+
+  openOrderDetails(order: any): void {
+    this.modalService.openModal(order);
   }
 }
