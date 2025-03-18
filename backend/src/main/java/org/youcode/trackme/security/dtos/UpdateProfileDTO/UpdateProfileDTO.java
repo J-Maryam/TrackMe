b609af794a41270d0cf37e1300fdd35148abc388
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2025-03-18 14:41:01 +0000

    feat(updateProfile dto): added UpdateProfileDTO

--- backend/src/main/java/org/youcode/trackme/security/dtos/UpdateProfileDTO/UpdateProfileDTO.java
+++ backend/src/main/java/org/youcode/trackme/security/dtos/UpdateProfileDTO/UpdateProfileDTO.java
@@ -1,4 +1,8 @@
 package org.youcode.trackme.security.dtos.UpdateProfileDTO;
 
-public record UpdateProfileDTO() {
-}
+import jakarta.validation.constraints.NotBlank;
+
+public record UpdateProfileDTO(
+        @NotBlank String phoneNumber,
+        @NotBlank String address
+) {}
