f0c7d6bcfbd7ddec1873b9154d09cc737ed70e7c
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2025-03-18 14:18:46 +0000

    feat(enums): added BraceletStatus enum type

--- backend/src/main/java/org/youcode/trackme/entities/enums/BraceletStatus.java
+++ backend/src/main/java/org/youcode/trackme/entities/enums/BraceletStatus.java
@@ -1,9 +1,9 @@
 package org.youcode.trackme.entities.enums;
 
 public enum BraceletStatus {
-    ACTIVE,       // Bracelet allumé et connecté
-    INACTIVE,     // Bracelet éteint ou désactivé
-    LOW_BATTERY,  // Batterie faible
-    NO_SIGNAL,    // Aucune connexion
-    SOS_TRIGGERED // Alerte SOS envoyée
+    ACTIVE,
+    INACTIVE,
+    LOW_BATTERY,
+    NO_SIGNAL,
+    SOS_TRIGGERED
 }
