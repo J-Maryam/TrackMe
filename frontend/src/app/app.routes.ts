df380fdfd7e62b6da53099199bf396d4c5c102bf
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2025-03-18 12:18:24 +0000

    feat(routes): added bracelet-management and alerts routes in app.route

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,8 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
 import {HomeComponent} from './features/home/home.component';
 import {LoginComponent} from './features/auth/login/login.component';
 import {OrderComponent} from './features/order/order.component';
-// import {AdminDashboardComponent} from './features/admin/admin-dashboard/admin-dashboard.component';
+import {AdminDashboardComponent} from './features/admin/admin-dashboard/admin-dashboard.component';
 import {ClientDashboardComponent} from './features/client/client-dashboard/client-dashboard.component';
 import {AuthGuard} from './core/guards/auth.guard';
 import {UserManagementComponent} from './features/admin/user-management/user-management.component';
@@ -12,22 +12,31 @@ import {RoleGuard} from './core/guards/role.guard';
 import {ProfileComponent} from './features/profile/profile.component';
 import {AssignedBraceletsComponent} from './features/client/assigned-bracelets/assigned-bracelets.component';
 import {TrackPatientComponent} from './features/client/track-patient/track-patient.component';
+import {AlertsComponent} from './features/client/alerts/alerts.component';
+import {BraceletManagementComponent} from './features/admin/bracelet-management/bracelet-management.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent, pathMatch: 'full' },
-  { path: 'login', component: LoginComponent },
-  { path: 'commande', component: OrderComponent },
-  // { path: 'admin-dashboard', component: AdminDashboardComponent, canActivate: [AuthGuard] },
-  { path: 'dashboard', component: ClientDashboardComponent, canActivate: [AuthGuard] },
-  { path: 'user-management', component: UserManagementComponent, canActivate: [AuthGuard] },
-  { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard] },
+  {path: '', component: HomeComponent, pathMatch: 'full'},
+  {path: 'login', component: LoginComponent},
+  {path: 'commande', component: OrderComponent},
+  {path: 'admin-dashboard', component: AdminDashboardComponent, canActivate: [AuthGuard]},
+  {path: 'dashboard', component: ClientDashboardComponent, canActivate: [AuthGuard]},
+  {path: 'user-management', component: UserManagementComponent, canActivate: [AuthGuard]},
+  {path: 'profile', component: ProfileComponent, canActivate: [AuthGuard]},
   {
     path: 'order-management',
     component: OrderListComponent,
     canActivate: [AuthGuard, RoleGuard],
-    data: { roles: ['ROLE_ADMIN'] },
+    data: {roles: ['ROLE_ADMIN']},
   },
-  { path: 'forbidden', component: ForbiddenComponent },
-  { path: 'assigned-bracelets', component: AssignedBraceletsComponent },
-  { path: 'patient-tracking/:patientId', component: TrackPatientComponent },
+  {
+    path: 'bracelet-management',
+    component: BraceletManagementComponent,
+    canActivate: [AuthGuard, RoleGuard],
+    data: {roles: ['ROLE_ADMIN']},
+  },
+  {path: 'forbidden', component: ForbiddenComponent},
+  {path: 'assigned-bracelets', component: AssignedBraceletsComponent},
+  {path: 'patient-tracking/:patientId', component: TrackPatientComponent},
+  {path: 'alerts', component: AlertsComponent},
 ];
