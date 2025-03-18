0f5df427da81a1adbb8e72150bd9f5e3ae055df8
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2025-03-18 14:38:42 +0000

    refactor(auth dto): added user info to ResponseLoginDTO

--- backend/src/main/java/org/youcode/trackme/security/dtos/AuthDTO/ResponseLoginDTO.java
+++ backend/src/main/java/org/youcode/trackme/security/dtos/AuthDTO/ResponseLoginDTO.java
@@ -4,6 +4,7 @@ import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.youcode.trackme.security.dtos.AppRoleDTO.EmbeddableAppRoleDTO;
 
 @Data
 @AllArgsConstructor
@@ -12,5 +13,13 @@ public class ResponseLoginDTO {
 
     private String email;
     private String role;
+    private Long id;
     private String token;
+
+    private String username;
+    private String address;
+    private String phoneNumber;
+
+    private boolean enabled;
+
 }
