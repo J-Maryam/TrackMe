c0347bc244f0caed63dfa95a2ff625e6152892e6
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2025-03-18 14:18:04 +0000

    feat(enums): added alertType enum type

--- backend/src/main/java/org/youcode/trackme/entities/enums/AlertType.java
+++ backend/src/main/java/org/youcode/trackme/entities/enums/AlertType.java
@@ -1,7 +1,9 @@
 package org.youcode.trackme.entities.enums;
 
-public enum AlertStatus {
-    OUT_OF_SAFE_ZONE,
-    BACK_IN_SAFE_ZONE,
-    NEW_ALERT
+public enum AlertType {
+    OUT_OF_SAFE_ZONE,    // Patient hors de la zone autorisée
+    BACK_IN_SAFE_ZONE,   // Patient revenu dans la zone
+    LOW_BATTERY,         // Batterie faible
+    NO_SIGNAL,           // Perte de signal
+    SOS_TRIGGERED        // Alerte SOS déclenchée
 }
