713fc4c86accdad528b11f8acc6f6c237dc912ec
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2025-03-18 14:29:39 +0000

    feat(user): added findByEmail method in UserRepository

--- backend/src/main/java/org/youcode/trackme/repositories/UserRepository.java
+++ backend/src/main/java/org/youcode/trackme/repositories/UserRepository.java
@@ -4,7 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.youcode.trackme.security.entities.AppUser;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<AppUser, Long> {
     boolean existsByEmail(String email);
+    Optional<AppUser> findByEmail(String email);
 }
