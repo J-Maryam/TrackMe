92ea2ef7f8e0087cd87b49fca93efc82599c46c6
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2025-03-18 22:06:10 +0000

    feat(admin service): implements getStats method to get the statistics in admin dashboard

--- backend/src/main/java/org/youcode/trackme/services/impls/AdminServiceImpl.java
+++ backend/src/main/java/org/youcode/trackme/services/impls/AdminServiceImpl.java
@@ -1,4 +1,35 @@
 package org.youcode.trackme.services.impls;
 
-public class AdminServiceImpl {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.youcode.trackme.dtos.AdminStatsDTO.AdminStatsDTO;
+import org.youcode.trackme.entities.enums.AlertStatus;
+import org.youcode.trackme.entities.enums.BraceletStatus;
+import org.youcode.trackme.repositories.AlertRepository;
+import org.youcode.trackme.repositories.BraceletRepository;
+import org.youcode.trackme.repositories.UserRepository;
+import org.youcode.trackme.services.AdminService;
+
+@Service
+@Transactional
+public class AdminServiceImpl implements AdminService {
+    @Autowired
+    private BraceletRepository braceletRepository;
+
+    @Autowired
+    private AlertRepository alertRepository;
+
+    @Autowired
+    private UserRepository clientRepository;
+
+    @Override
+    public AdminStatsDTO getStats() {
+        int activeBracelets = (int) braceletRepository.countByStatus(BraceletStatus.ACTIVE);
+        int inactiveBracelets = (int) braceletRepository.countByStatus(BraceletStatus.INACTIVE);
+        int pendingAlerts = (int) alertRepository.countByStatus(AlertStatus.UNRESOLVED);
+        int totalClients = (int) clientRepository.countByRoleId(2L);
+
+        return new AdminStatsDTO(activeBracelets, inactiveBracelets, pendingAlerts, totalClients);
+    }
 }
