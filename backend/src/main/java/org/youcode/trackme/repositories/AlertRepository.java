88e7b79b8ca7303732ce830d09cd05a6cfffebf1
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2025-03-18 14:27:15 +0000

    feat(alert): added findByStatus, findByType and findByBraceletId methods in AlertRepository

--- backend/src/main/java/org/youcode/trackme/repositories/AlertRepository.java
+++ backend/src/main/java/org/youcode/trackme/repositories/AlertRepository.java
@@ -3,8 +3,14 @@ package org.youcode.trackme.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.youcode.trackme.entities.Alert;
-import org.youcode.trackme.entities.Patient;
+import org.youcode.trackme.entities.enums.AlertStatus;
+import org.youcode.trackme.entities.enums.AlertType;
+
+import java.util.List;
 
 @Repository
 public interface AlertRepository extends JpaRepository<Alert, Long> {
+    List<Alert> findByStatus(AlertStatus status);
+    List<Alert> findByType(AlertType type);
+    List<Alert> findByBraceletId(Long braceletId);
 }
