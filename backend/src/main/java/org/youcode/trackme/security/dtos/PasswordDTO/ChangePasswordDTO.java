85a2b999a8f6033fa5e4e6ef749edec8cf018685
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2025-03-18 14:39:35 +0000

    refactor(password dto): added validation in ChanegdPasswordDTO

--- backend/src/main/java/org/youcode/trackme/security/dtos/PasswordDTO/ChangePasswordDTO.java
+++ backend/src/main/java/org/youcode/trackme/security/dtos/PasswordDTO/ChangePasswordDTO.java
@@ -1,6 +1,7 @@
 package org.youcode.trackme.security.dtos.PasswordDTO;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,9 +11,10 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class ChangePasswordDTO {
 
-    @NotBlank
+    @NotBlank(message = "L’ancien mot de passe est requis")
     private String oldPassword;
 
-    @NotBlank
+    @NotBlank(message = "Le nouveau mot de passe est requis")
+    @Size(min = 8, message = "Le mot de passe doit contenir au moins 8 caractères")
     private String newPassword;
 }
