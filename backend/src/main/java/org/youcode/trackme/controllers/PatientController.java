62c2d1da643fb46578da3488e4da72e131bd9871
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2025-03-18 14:13:59 +0000

    feat(alert) added alert dto

--- backend/src/main/java/org/youcode/trackme/controllers/PatientController.java
+++ backend/src/main/java/org/youcode/trackme/controllers/PatientController.java
@@ -1,40 +1,18 @@
 package org.youcode.trackme.controllers;
 
-import jakarta.validation.Valid;
-import org.springframework.data.domain.Pageable;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.youcode.trackme.common.ApiResponse;
-import org.youcode.trackme.common.PagedResponse;
 import org.youcode.trackme.common.controllers.GenericControllerImpl;
 import org.youcode.trackme.dtos.patient.PatientRequestDTO;
 import org.youcode.trackme.dtos.patient.PatientResponseDTO;
 import org.youcode.trackme.entities.Patient;
-import org.youcode.trackme.security.dtos.AppUserDTO.CreateAppUserDTO;
-import org.youcode.trackme.security.dtos.AppUserDTO.ResponseAppUserDTO;
 import org.youcode.trackme.services.PatientService;
 
-import java.util.List;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/public/patients")
 @CrossOrigin(origins = "http://localhost:4200")
 public class PatientController extends GenericControllerImpl<Patient, Long, PatientRequestDTO, PatientResponseDTO> {
     public PatientController(PatientService service) {
         super(service);
-    }
-
-    @PostMapping("/public/patients")
-    public ResponseEntity<PatientResponseDTO> createPatient(@Valid @RequestBody PatientRequestDTO patientRequestDTO) {
-        PatientResponseDTO patient = service.create(patientRequestDTO);
-        return new ResponseEntity<>(patient, HttpStatus.CREATED);
-    }
-
-    @GetMapping("/public/patients")
-    public ResponseEntity<ApiResponse<PagedResponse<PatientResponseDTO>>> getAll(Pageable pageable) {
-        PagedResponse<PatientResponseDTO> patients = service.getAll(pageable);
-        System.out.println("patients: " + patients);
-        return ResponseEntity.ok(ApiResponse.success(patients, "Data retrieved successfully"));
     }
 }
--- backend/src/main/java/org/youcode/trackme/dtos/alert/AlertDTO.java
+++ backend/src/main/java/org/youcode/trackme/dtos/alert/AlertDTO.java
@@ -1,4 +1,14 @@
 package org.youcode.trackme.dtos.alert;
 
-public record AlertDTO() {
-}
+import org.youcode.trackme.dtos.bracelet.EmbeddableBraceletResponseDTO;
+import org.youcode.trackme.entities.enums.AlertStatus;
+
+import java.time.LocalDateTime;
+
+public record AlertDTO(
+        Long id,
+        String message,
+        LocalDateTime timestamp,
+        AlertStatus status,
+        EmbeddableBraceletResponseDTO bracelet
+) {}
