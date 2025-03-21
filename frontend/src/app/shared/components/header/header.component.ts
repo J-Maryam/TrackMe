32ce93ccaf4f19216fd1e9d5310cf39670e54c2c
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2025-03-21 10:21:32 +0000

    refactor(homepage): modifier homepage pour defiler entre les sections du homapage

--- frontend/src/app/shared/components/header/header.component.ts
+++ frontend/src/app/shared/components/header/header.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import {AuthComponent} from '../../../features/auth/auth.component';
-import {RouterLink} from '@angular/router';
+import { AuthComponent } from '../../../features/auth/auth.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -13,4 +13,38 @@ import {RouterLink} from '@angular/router';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  scrollToHome() {
+    const element = document.getElementById('home');
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
+
+  scrollToFeatures() {
+    const element = document.getElementById('features');
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
+
+  scrollToAbout() {
+    const element = document.getElementById('about');
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
+
+  scrollToFaq() {
+    const element = document.getElementById('faq');
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
+
+  scrollToContact() {
+    const element = document.getElementById('contact');
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
 }
