2555ab0af4554d4320f983fa87896f404e8c1ad2
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2025-03-25 12:06:29 +0000

    feat(client dashboard): updated client dashboard

--- frontend/src/app/features/client/client-dashboard/client-dashboard.component.ts
+++ frontend/src/app/features/client/client-dashboard/client-dashboard.component.ts
@@ -1,18 +1,23 @@
-import { Component } from '@angular/core';
-import {SidebarComponent} from '../../../shared/components/sidebar/sidebar.component';
+import { Component, AfterViewInit } from '@angular/core';
+import { SidebarComponent } from '../../../shared/components/sidebar/sidebar.component';
+import * as L from 'leaflet';
+import {NgForOf, NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-client-dashboard',
-  imports: [
-    SidebarComponent
-  ],
+  imports: [SidebarComponent, NgIf, NgForOf],
   templateUrl: './client-dashboard.component.html',
   standalone: true,
-  styleUrl: './client-dashboard.component.css'
+  styleUrls: ['./client-dashboard.component.css']
 })
 export class ClientDashboardComponent {
-  activeBracelets = 3;
-  recentAlerts = 2;
-  lowBattery = 1;
-  lastLocation = 'Paris, France';
+  activeBracelets = 7;
+  recentAlerts = 4;
+  batteryAverage = 82;
+
+  patientLocations: { lat: number; lng: number; name: string }[] = [
+    { lat: 48.8566, lng: 2.3522, name: 'Patient 1 - Paris' },
+    { lat: 51.5074, lng: -0.1278, name: 'Patient 2 - Londres' },
+    { lat: 40.7128, lng: -74.0060, name: 'Patient 3 - New York' }
+  ];
 }
